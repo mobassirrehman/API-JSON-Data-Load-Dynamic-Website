727545fe4b066355de7c10481b3fee7718c2dfdf
Mobassir Rehman <mobassir.rehman@Mobassirs-MacBook-Pro.local>
2025-09-06 07:24:22 +0600

    Display Post Card

--- posts.js
+++ posts.js
@@ -1,29 +1,37 @@
 const loadPost = () => {
-    const url = "https://jsonplaceholder.typicode.com/posts";
-    fetch(url)
+  const url = "https://jsonplaceholder.typicode.com/posts";
+  fetch(url)
     .then((res) => res.json())
     .then((data) => {
-        // console.log(data);
-        displayPosts(data);
+      // console.log(data);
+      displayPosts(data);
     });
 };
+
+// {
+//     "userId": 10,
+//     "id": 98,
+//     "title": "laboriosam dolor voluptates",
+//     "body": "doloremque ex facilis sit sint culpa\nsoluta assumenda eligendi non ut
+//  eius\nsequi ducimus vel quasi\nveritatis est dolores"
+// }
 
 const displayPosts = (posts) => {
+  //   get the container and empty the conatiner
+  const postsContainer = document.getElementById("post-container");
+  postsContainer.innerHTML = "";
+  posts.forEach((post) => {
+    //2.create element
+    const postCard = document.createElement("div");
+    postCard.innerHTML = `<div class="post-card">
+        <h2>${post.title}</h2>
+        <p>
+        ${post.body}
+        </p>
+      </div>`;
 
-    // get the container
-    const postContainer = document.getElementById("post-container");
-    postContainer.innerHTML = "";
-
-    posts.forEach(post => {
-        console.log(post.title);
-        // create HTML element
-        const li = document.createElement("li");
-        li.innerText = post.title
-        console.log(li);
-
-        // add li into container
-        postContainer.appendChild(li)
-    });
-
-   
+    //   add to the container
+    postsContainer.append(postCard);
+  });
 };
+loadPost();
